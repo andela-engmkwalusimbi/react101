70fd772e37fe22aa5b15d3d9f02b17e353641284
Mahad Walusimbi <engmk123@gmail.com>
2016-07-25 21:47:40 +0300

    Inline styles

--- Components/App.jsx
+++ Components/App.jsx
@@ -2,9 +2,13 @@ import React, {Component} from 'react';
 
 class App extends Component {
     render(){
+        var myStyle = {
+            fontSize: 100,
+            color: '#ccce63'
+        }
         return (
             <div>
-                <h1>Heading</h1>
+                <h1 style={myStyle}>Heading</h1>
                 <p>Paragraph one</p>
             </div>
         );
